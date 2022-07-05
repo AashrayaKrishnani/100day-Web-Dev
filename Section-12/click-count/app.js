dbd7a833686cae15ecb7fdee35ad8c61293bf663
AashrayaKrishnani <loveaash3@gmail.com>
2022-07-05 22:50:09 +0530

    Initial Commit

--- Section-12/click-count/app.js
+++
@@ -1,9 +0,0 @@
-let p = document.querySelector("p");
-let clickCount = 0;
-
-function changePText() {
-  clickCount++;
-  p.textContent = "Clicked " + clickCount + " times!";
-}
-
-p.addEventListener("click", changePText);
--- Section-12/input-text/app.js
+++
@@ -1,9 +0,0 @@
-let p = document.querySelector('p');
-
-function handleInput(event){
-    let inputText = event.target.value;
-    p.textContent = inputText;
-    console.dir(event);
-}
-
-inputField.addEventListener('input', handleInput);
