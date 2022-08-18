6990638cadd948abbf7736cdb3db1aa2cce7f839
Aashraya Krishnani <loveaash3@gmail.com>
2022-08-18 23:09:40 +0530

    D79, ejs 'includes' files, css variables + theme

--- Section-32/app.js
+++ Section-32/app.js
@@ -9,6 +9,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use("/public", express.static("public"));
+
 app.use(authRoutes);
 
 app.listen(3000);
